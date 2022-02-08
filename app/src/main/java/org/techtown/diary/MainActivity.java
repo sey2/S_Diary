3cd82cf7016a6dc4176e390aa22b6bd215e78d18
sey2 <casio2978@naver.com>
2022-02-09 00:08:52 +0900

    [Update] 프래그먼트 인플레이션 소스 코드 추가 (22.02.08)

--- app/src/main/java/org/techtown/diary/MainActivity.java
+++ app/src/main/java/org/techtown/diary/MainActivity.java
@@ -1,14 +1,69 @@
 package org.techtown.diary;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.tabs.TabLayout;
+
+
+/* OnTabSelectedListener -> 하나의 프래그먼트에서 다른 프래그먼트로 전환하는 용도 */
+public class MainActivity extends AppCompatActivity implements OnTabSelectedListener {
+
+    Fragment1 fragment1;
+    Fragment2 fragment2;
+    Fragment3 fragment3;
+
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+        fragment3 = new Fragment3();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment1).commit();
+
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener(){
+                    @Override   // 하단 탭 버튼을 눌렀을 때 호출되는 메서드
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+                        switch (item.getItemId()){
+                            case R.id.tab1:
+                                Toast.makeText(getApplicationContext(), "첫 번째 탭 선택됨", Toast.LENGTH_LONG).show();
+                                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+
+                                return true;
+                            case R.id.tab2:
+                                Toast.makeText(getApplicationContext(), "두 번째 탭 선택됨", Toast.LENGTH_LONG).show();
+                                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment2).commit();
+
+                                return true;
+                            case R.id.tab3:
+                                Toast.makeText(getApplicationContext(), "세 번째 탭 선택됨", Toast.LENGTH_LONG).show();
+                                getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment3).commit();
+
+                                return true;
+                        }
+                        return false;
+                    }
+                });
+    }
+
+    /* 이 메서드가 호출되면 하단 탭의 setSelected 메서드를 이용해 다른 탭 버튼이 선택 되도록 함 */
+    public void onTabSelected(int position){
+        if (position == 0)
+            bottomNavigation.setSelectedItemId(R.id.tab1);
+        else if (position == 1)
+            bottomNavigation.setSelectedItemId(R.id.tab2);
+        else if (position == 2)
+            bottomNavigation.setSelectedItemId(R.id.tab3);
     }
 }
