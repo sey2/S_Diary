31edeb8cb68a5360233479f1481e139ac595d769
sey2 <casio2978@naver.com>
2022-02-09 00:07:02 +0900

    [Add] 메인화면 구성을 위한 fragment 파일 추가 (22.02.08)

---
+++ app/src/main/java/org/techtown/diary/Fragment3.java
@@ -0,0 +1,24 @@
+package org.techtown.diary;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment3 extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        ViewGroup rootView  = (ViewGroup) inflater.inflate(R.layout.fragment3, container, false);
+
+        initUI(rootView);
+
+        return rootView;
+    }
+
+    // XML 레이아웃 안에 들어 있는 위젯이나 레이아웃을 찾아 변수에 할당하기 위한 메서드
+    private void initUI(ViewGroup rootView){
+
+    }
+}
