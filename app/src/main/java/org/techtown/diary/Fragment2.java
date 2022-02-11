8e313a320409db48388a9b988fbd6fb8ac3ae32c
casio2978 <casio2978@naver.com>
2022-02-11 15:51:03 +0900

    [Update] 일기 작성 화면 컴포넌트 이벤트 처리 코드 추가 (22.02.11)

--- app/src/main/java/org/techtown/diary/Fragment2.java
+++ app/src/main/java/org/techtown/diary/Fragment2.java
@@ -1,13 +1,21 @@
 package org.techtown.diary;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
+import com.github.channguyen.rsv.RangeSliderView;
+
 public class Fragment2 extends Fragment {
+
+    Context context;
+    OnTabSelectedListener listener;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
@@ -18,8 +26,63 @@ public class Fragment2 extends Fragment {
         return rootView;
     }
 
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+
+        this.context = context;
+
+        if(context instanceof OnTabSelectedListener)
+            listener = (OnTabSelectedListener) context;
+     }
+
+     @Override
+     public void onDetach(){
+        super.onDetach();
+
+        if(context!=null){
+            context = null;
+            listener = null;
+        }
+     }
+
     // XML 레이아웃 안에 들어 있는 위젯이나 레이아웃을 찾아 변수에 할당하기 위한 메서드
     private void initUI(ViewGroup rootView){
+        Button saveButton = rootView.findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(listener != null)
+                    listener.onTabSelected(0);
+            }
+        });
 
+        Button deleteButton = rootView.findViewById(R.id.deleteButton);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(listener != null)
+                    listener.onTabSelected(0);
+            }
+        });
+
+        Button closeButton = rootView.findViewById(R.id.closeButton);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(listener!= null)
+                    listener.onTabSelected(0);
+            }
+        });
+
+        RangeSliderView sliderView = rootView.findViewById(R.id.sliderView);
+        sliderView.setOnSlideListener(new RangeSliderView.OnSlideListener() {
+            @Override
+            public void onSlide(int index) {
+                Toast.makeText(context, "moodIndex changed to " + index, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        sliderView.setInitialIndex(2);  // 슬라이더 인덱스 설정
     }
 }
