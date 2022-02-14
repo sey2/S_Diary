b0376c8bec2030ba6280cf0bb4a52d1cc1db5d2b
sey2 <casio2978@naver.com>
2022-02-14 19:55:25 +0900

    [Update] 데이터베이스 코드 추가 (02.14)

--- app/src/main/java/org/techtown/diary/db/NoteDatabase.java
+++ app/src/main/java/org/techtown/diary/db/NoteDatabase.java
@@ -8,122 +8,185 @@ import android.util.Log;
 
 import org.techtown.diary.data.AppConstants;
 
+/**
+ * 메모 데이터베이스
+ */
 public class NoteDatabase {
     private static final String TAG = "NoteDatabase";
 
+    /**
+     * 싱글톤 인스턴스
+     */
     private static NoteDatabase database;
+
+    /**
+     * table name for MEMO
+     */
     public static String TABLE_NOTE = "NOTE";
+
+    /**
+     * version
+     */
     public static int DATABASE_VERSION = 1;
 
+
+    /**
+     * Helper class defined
+     */
     private DatabaseHelper dbHelper;
+
+    /**
+     * SQLiteDatabase 인스턴스
+     */
     private SQLiteDatabase db;
+
+    /**
+     * 컨텍스트 객체
+     */
     private Context context;
 
-    private NoteDatabase(Context context){
+    /**
+     * 생성자
+     */
+    private NoteDatabase(Context context) {
         this.context = context;
     }
 
-    public static NoteDatabase getInstance(Context context){
-        if(database == null)
+    /**
+     * 인스턴스 가져오기
+     */
+    public static NoteDatabase getInstance(Context context) {
+        if (database == null) {
             database = new NoteDatabase(context);
+        }
 
         return database;
     }
 
-    public boolean open(){
-        println("opening database [" + AppConstants.DATABASE_NAME + "]");
+    /**
+     * 데이터베이스 열기
+     */
+    public boolean open() {
+        println("opening database [" + AppConstants.DATABASE_NAME + "].");
 
         dbHelper = new DatabaseHelper(context);
         db = dbHelper.getWritableDatabase();
+
         return true;
     }
 
-    public void close(){
-        println("closing database [" + AppConstants.DATABASE_NAME + "]" );
+    /**
+     * 데이터베이스 닫기
+     */
+    public void close() {
+        println("closing database [" + AppConstants.DATABASE_NAME + "].");
         db.close();
 
         database = null;
     }
 
-    public Cursor rawQuery(String SQL){
+    /**
+     * execute raw query using the input SQL
+     * close the cursor after fetching any result
+     *
+     * @param SQL
+     * @return
+     */
+    public Cursor rawQuery(String SQL) {
         println("\nexecuteQuery called.\n");
 
-        Cursor cursor = null;
-        try{
-            cursor = db.rawQuery(SQL,null);
-            println("cursor count : " + cursor.getCount());
-        }catch (Exception ex){
-            Log.e(TAG,"Exception in executeQuery", ex);
+        Cursor c1 = null;
+        try {
+            c1 = db.rawQuery(SQL, null);
+            println("cursor count : " + c1.getCount());
+        } catch(Exception ex) {
+            Log.e(TAG, "Exception in executeQuery", ex);
         }
 
-        return cursor;
+        return c1;
     }
 
-    public boolean execSQL(String SQL){
+    public boolean execSQL(String SQL) {
         println("\nexecute called.\n");
 
-        try{
-            Log.d(TAG,"SQL : " + SQL);
+        try {
+            Log.d(TAG, "SQL : " + SQL);
             db.execSQL(SQL);
-        }catch (Exception ex){
-            Log.e(TAG,"Exception in executeQuery", ex);
+        } catch(Exception ex) {
+            Log.e(TAG, "Exception in executeQuery", ex);
             return false;
         }
 
         return true;
     }
 
+
+
+    /**
+     * Database Helper inner class
+     */
     private class DatabaseHelper extends SQLiteOpenHelper {
 
-        public DatabaseHelper(Context context){
+        public DatabaseHelper(Context context) {
             super(context, AppConstants.DATABASE_NAME, null, DATABASE_VERSION);
         }
 
-        @Override
-        public void onCreate(SQLiteDatabase db){
-            println("creating database3 [" + AppConstants.DATABASE_NAME + "]");
+        public void onCreate(SQLiteDatabase db) {
+            println("creating database [" + AppConstants.DATABASE_NAME + "].");
+
+            // TABLE_NOTE
             println("creating table [" + TABLE_NOTE + "].");
 
-            String DROP_SQL = "drop table if exists " +TABLE_NOTE;
-
-            try{
+            // drop existing table
+            String DROP_SQL = "drop table if exists " + TABLE_NOTE;
+            try {
                 db.execSQL(DROP_SQL);
-            }catch (Exception e){
-                Log.e(TAG, "Exception in DROP_SQL", e);
+            } catch(Exception ex) {
+                Log.e(TAG, "Exception in DROP_SQL", ex);
             }
 
+            // create table
             String CREATE_SQL = "create table " + TABLE_NOTE + "("
-                    + " _id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
-                    + " WEATHER TEXT DEFAULT '', "
-                    + " ADDRESS TEXT DEFAULT '', "
-                    + " LOCATION_X TEXT DEFAULT '', "
-                    + " LOCATION_Y TEXT DEFAULT '', "
-                    + " CONTENTS TEXT DEFAULT '', "
-                    + " MOOD TEXT, "
-                    + " PICTURE TEXT DEFAULT '', "
-                    + " CREATE_DATE TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "
-                    + " MODIFY_DATE TIMESTAMP DEFAULT CURRENT_TIMESTAMP"
-                    +")";
-
-            try{
+                    + "  _id INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                    + "  WEATHER TEXT DEFAULT '', "
+                    + "  ADDRESS TEXT DEFAULT '', "
+                    + "  LOCATION_X TEXT DEFAULT '', "
+                    + "  LOCATION_Y TEXT DEFAULT '', "
+                    + "  CONTENTS TEXT DEFAULT '', "
+                    + "  MOOD TEXT, "
+                    + "  PICTURE TEXT DEFAULT '', "
+                    + "  CREATE_DATE TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "
+                    + "  MODIFY_DATE TIMESTAMP DEFAULT CURRENT_TIMESTAMP "
+                    + ")";
+            try {
                 db.execSQL(CREATE_SQL);
-            }catch (Exception e){
-                Log.e(TAG, "Exception in CREATE_INDEX_SQL", e);
+            } catch(Exception ex) {
+                Log.e(TAG, "Exception in CREATE_SQL", ex);
+            }
+
+            // create index
+            String CREATE_INDEX_SQL = "create index " + TABLE_NOTE + "_IDX ON " + TABLE_NOTE + "("
+                    + "CREATE_DATE"
+                    + ")";
+            try {
+                db.execSQL(CREATE_INDEX_SQL);
+            } catch(Exception ex) {
+                Log.e(TAG, "Exception in CREATE_INDEX_SQL", ex);
             }
         }
 
-        public void onOpen(SQLiteDatabase db){
+        public void onOpen(SQLiteDatabase db) {
             println("opened database [" + AppConstants.DATABASE_NAME + "].");
         }
 
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-            println("Upgrading database from version " + oldVersion + " to " + newVersion +" .");
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            println("Upgrading database from version " + oldVersion + " to " + newVersion + ".");
         }
-
     }
 
-    private void println(String msg){
-        Log.d(TAG,msg);
+    private void println(String msg) {
+        Log.d(TAG, msg);
     }
+
 
 }
